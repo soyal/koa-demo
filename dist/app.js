0fe630010c7ea460aa66671f503b2e3afa368c8e
soyal <xiongxiao9958@gmail.com>
2017-12-29 17:34:17 +0800

    项目初始化环境搭建完成

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Koa = require("koa");
+const app = new Koa();
+app.use(ctx => {
+    ctx.body = 'Hello Koa';
+});
+app.listen(3000);
+//# sourceMappingURL=app.js.map
